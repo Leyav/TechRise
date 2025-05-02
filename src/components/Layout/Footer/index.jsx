f992e337f4936aaf7cf5a460015613d97e47ab67
leyavarghese <leya.annie@erstechnology.com>
2025-05-02 21:06:04 +0530

    Footer changes

--- src/components/Layout/Footer/index.jsx
+++ src/components/Layout/Footer/index.jsx
@@ -1,70 +1,154 @@
 import React from "react";
-import { FaFacebookF, FaInstagram, FaXTwitter } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+import { headerData } from "../Header/Navigation/MenuData";
+import { Icon } from "@iconify/react";
+
 
 const Footer = () => {
   return (
-    <footer className="bg-white px-6 py-12 border-t border-gray-200">
-      <div className="max-w-6xl mx-auto grid md:grid-cols-4 gap-10">
-        <div>
-          <div className="flex items-center gap-2 mb-4">
-            <img src="/images/logo/mLogo.svg" alt="Logo" className="w-8 h-8" />
-            <span className="font-bold text-lg">TechRise</span>
+    <footer className="pt-16 bg-darkmode">
+      <div className="container mx-auto lg:max-w-screen-xl md:max-w-screen-md px-4">
+        <div className="grid grid-cols-1 sm:grid-cols-5 lg:gap-20 md:gap-6 sm:gap-12 gap-6  pb-16">
+          <div className="col-span-2">
+          <img src="/images/logo/mLogo.svg" alt="Logo" className="w-8 h-8" />
+            <p className="text-xs font-medium text-grey dark:text-white/50 mt-5 mb-16 max-w-70%">
+              Open an account in minutes, get full financial
+              control for much longer.
+            </p>
+            <div className="flex gap-6 items-center">
+              <Link to="#" className="group bg-white hover:bg-primary rounded-full shadow-xl p-3">
+                <Icon
+                  icon="fa6-brands:facebook-f"
+                  width="16"
+                  height="16"
+                  className=" group-hover:text-white text-black"
+                />
+              </Link>
+              <Link to="#" className="group bg-white hover:bg-primary rounded-full shadow-xl p-3">
+                <Icon
+                  icon="fa6-brands:instagram"
+                  width="16"
+                  height="16"
+                  className=" group-hover:text-white text-black"
+                />
+              </Link>
+              <Link to="#" className="group bg-white hover:bg-primary rounded-full shadow-xl p-3">
+                <Icon
+                  icon="fa6-brands:x-twitter"
+                  width="16"
+                  height="16"
+                  className=" group-hover:text-white text-black"
+                />
+              </Link>
+            </div>
           </div>
-          <p className="text-sm text-gray-600 mb-6">
-            Open an account in minutes, get full financial control for much longer.
+          <div className="">
+            <h4 className="text-black dark:text-white mb-9 font-semibold text-xl">Company</h4>
+            <ul>
+              <li className="pb-5">
+                <Link
+                  to="#"
+                  className="text-black/70 dark:text-white/70 hover:text-primary dark:hover:text-primary text-base"
+                >
+                  About
+                </Link>
+              </li>
+              <li className="pb-5">
+                <Link
+                  to="#"
+                  className="text-black/70 dark:text-white/70 hover:text-primary dark:hover:text-primary text-base"
+                >
+                  Careers
+                </Link>
+              </li>
+              <li className="pb-5">
+                <Link
+                  to="#"
+                  className="text-black/70 dark:text-white/70 hover:text-primary dark:hover:text-primary text-base"
+                >
+                  Mobile
+                </Link>
+              </li>
+              <li className="pb-5">
+                <Link
+                  to="#"
+                  className="text-black/70 dark:text-white/70 hover:text-primary dark:hover:text-primary text-base"
+                >
+                  Blog
+                </Link>
+              </li>
+              <li className="pb-5">
+                <Link
+                  to="#"
+                  className="text-black/70 dark:text-white/70 hover:text-primary dark:hover:text-primary text-base"
+                >
+                  How we work?
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="">
+            <h4 className="text-black dark:text-white mb-9 font-semibold text-xl">Information</h4>
+            <ul>
+              <li className="pb-5">
+                <Link
+                  to="#"
+                  className="text-black/70 dark:text-white/70 dark:hover:text-primary hover:text-primary text-base"
+                >
+                  Help/FAQ
+                </Link>
+              </li>
+              <li className="pb-5">
+                <Link
+                  to="#"
+                  className="text-black/70 dark:text-white/70 dark:hover:text-primary hover:text-primary text-base"
+                >
+                  Press
+                </Link>
+              </li>
+              <li className="pb-5">
+                <Link
+                  to="#"
+                  className="text-black/70 dark:text-white/70 hover:text-primary dark:hover:text-primary text-base"
+                >
+                  Affiliates
+                </Link>
+              </li>
+              <li className="pb-5">
+                <Link
+                  to="#"
+                  className="text-black/70 dark:text-white/70 hover:text-primary dark:hover:text-primary text-base"
+                >
+                  Partners
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="">
+            <h4 className="text-black dark:text-white mb-9 font-semibold text-xl">More</h4>
+            <ul>
+              {headerData.map((item, index) => (
+                <li key={index} className="pb-4">
+                  <Link
+                    to={item.href}
+                    className="text-black/70 dark:text-white/70 dark:hover:text-primary hover:text-primary text-base"
+                  >
+                    {item.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div className="border-t border-grey/15 dark:border-white/15 py-10 flex justify-between items-center">
+          <p className="text-sm text-black/70 dark:text-white/70">
+          ©2025 - TechRise. Distributed and Developed By <Link to="https://leytech.com/" target="_blank" className="hover:text-primary">LeyTech</Link>
           </p>
-          <div className="flex gap-4">
-            <div className="w-10 h-10 bg-white rounded-full shadow flex items-center justify-center">
-              <FaFacebookF />
-            </div>
-            <div className="w-10 h-10 bg-white rounded-full shadow flex items-center justify-center">
-              <FaInstagram />
-            </div>
-            <div className="w-10 h-10 bg-white rounded-full shadow flex items-center justify-center">
-              <FaXTwitter />
-            </div>
+
+          <div className="">
+            <Link to="#" className="text-sm text-black/70 dark:text-white/70 px-5 border-r border-grey/15 dark:border-white/15 hover:text-primary dark:hover:text-primary">Privacy policy</Link>
+            <Link to="#" className="text-sm text-black/70 dark:text-white/70 px-5 hover:text-primary dark:hover:text-primary">Terms & conditions</Link>
           </div>
-        </div>
-
-        <div>
-          <h3 className="font-semibold mb-4">Company</h3>
-          <ul className="space-y-2 text-gray-600 text-sm">
-            <li>About</li>
-            <li>Careers</li>
-            <li>Mobile</li>
-            <li>Blog</li>
-            <li>How we work?</li>
-          </ul>
-        </div>
-
-        <div>
-          <h3 className="font-semibold mb-4">Information</h3>
-          <ul className="space-y-2 text-gray-600 text-sm">
-            <li>Help/FAQ</li>
-            <li>Press</li>
-            <li>Affiliates</li>
-            <li>Partners</li>
-          </ul>
-        </div>
-
-        <div>
-          <h3 className="font-semibold mb-4">More</h3>
-          <ul className="space-y-2 text-gray-600 text-sm">
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Categories</li>
-            <li>Courses</li>
-          </ul>
-        </div>
-      </div>
-
-      <div className="mt-10 border-t pt-6 text-sm text-gray-500 flex flex-col md:flex-row justify-between items-center">
-        <p>
-          ©2025 - TechRise. Distributed and Developed By LeyTech
-        </p>
-        <div className="flex gap-4 mt-2 md:mt-0">
-          <span>Privacy policy</span>
-          <span>Terms & conditions</span>
         </div>
       </div>
     </footer>
